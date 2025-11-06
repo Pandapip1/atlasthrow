af29bd48b98700a8e6b7ed19a0a110870e5039ad
Gavin John <gavinnjohn@gmail.com>
2025-11-05 19:43:59 -0800

    Add hw4code and hw5code

---
+++ src/hw4code/hw4code/hw4p1.py
@@ -0,0 +1,71 @@
+'''hw4p1.py
+
+   This is skeleton code for HW4 Problem 1.  PLEASE EDIT (SEARCH FOR FIXME)!
+
+   This should simply use NumPy to implement the known forward
+   kinematics and Jacobian functions for the 3 DOF robot.
+
+'''
+
+import numpy as np
+
+
+#
+#  Forward Kinematics
+#
+def fkin(q):
+    # Compute the tip position.
+
+    FIXME - Put the forward kinematics here.  Below just illustrates NumPy.
+
+    x = np.array([1,
+                  2,
+                  3])
+
+    # Return the tip position as a NumPy 3-element vector.
+    return x
+
+
+#
+#  Jacobian
+#
+def Jac(q):
+    # Compute the Jacobian for the tip position.
+
+    FIXME - Put the Jacobian here.  Below again illustrates NumPy.
+    
+    J = np.eye(3)
+    J = np.random.rand(3,3)
+    J = np.array([[1, 2, 3],
+                  [4, 5, 6],
+                  [7, 8, 9]])
+
+    # Return the Jacobian as a NumPy 3x3 matrix.
+    return J
+
+
+#
+#  Main Code
+#
+#  This simply tests the above functions.
+#
+def main():
+    # Run the test case.  Suppress infinitesimal numbers.
+    np.set_printoptions(suppress=True)
+
+    # First (given) test case with following joint coordinates.
+    print("TEST CASE #1:")
+    q = np.radians(np.array([20, 40, -30]))
+    print('q:\n',       q)
+    print('fkin(q):\n', fkin(q))
+    print('Jac(q):\n',  Jac(q))
+
+    # Second test case with following joint coordinates.
+    print("TEST CASE #2")
+    q = np.radians(np.array([30, 20, 50]))
+    print('q:\n',       q)
+    print('fkin(q):\n', fkin(q))
+    print('Jac(q):\n',  Jac(q))
+
+if __name__ == "__main__":
+    main()
---
+++ src/hw4code/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = 'hw4code'
+
+# Create a mapping of other files to be copied in the src -> install
+# build.  This is a list of tuples.  The first entry in the tuple is
+# the install folder into which to place things.  The second entry is
+# a list of files to place into that folder.
+otherfiles = [
+]
+
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ]+otherfiles,
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robot',
+    maintainer_email='robot@todo.todo',
+    description='The 133a HW4 Code',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'hw4p1      = hw4code.hw4p1:main',
+            'hw4p2      = hw4code.hw4p2:main',
+        ],
+    },
+)
