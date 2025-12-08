61d1cb715717b3ce03e2b8ed82bb5046dc15c170
Gavin John <gavinnjohn@gmail.com>
2025-12-08 14:02:25 -0800

    Never recalculate ikin

--- src/atlassquat/atlassquat/atlassquat.py
+++ src/atlassquat/atlassquat/atlassquat.py
@@ -259,9 +259,9 @@ class TrajectoryNode(Node):
         tReturn = self.periodThrow
 
         if t2 < tThrow:
-            recalculate_ikin_every_sec = 1
-            if (self.t // self.dt) % (recalculate_ikin_every_sec // self.dt) == 0:
-                (self.qFinThrow, self.qdotFinThrow) = self.chain.relative_ikin(self.chain, self.leftFootLink, self.rightHandLink, pd=self.pRHThrow, vd=self.vRHThrow, q_init=self.qc)
+            # recalculate_ikin_every_sec = 1
+            # if (self.t // self.dt) % (recalculate_ikin_every_sec // self.dt) == 0:
+            #     (self.qFinThrow, self.qdotFinThrow) = self.chain.relative_ikin(self.chain, self.leftFootLink, self.rightHandLink, pd=self.pRHThrow, vd=self.vRHThrow, q_init=self.qc)
 
             (pdRightHand, vdRightHand) = spline(t2 - tI, tThrow - tI, self.pRH0, self.pRHThrow, np.zeros(3), np.array(self.vRHThrow))
 
